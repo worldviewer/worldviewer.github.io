7ec5a4bc676321d99b94ad5bdd411a53c09f3dd1
Chris Reeve <paradigmsareconstructed@gmail.com>
2015-12-27 22:12:17 -0800

    Removed card titles

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -3518,7 +3518,10 @@ $(document).ready(function(){
 
     $(document).on('click.card', '.card', function (e) {
       if ($(this).find('> .card-reveal').length) {
-        if ($(e.target).is($('.card-reveal .card-title')) || $(e.target).is($('.card-reveal .card-title i'))) {
+
+      	// CUSTOM: Altered the click target
+        if ($(e.target).is($('.card-reveal .card-text')) || $(e.target).is($('.card-reveal .card-text i'))) {
+
           // Make Reveal animate down and display none
           $(this).find('.card-reveal').velocity(
             {translateY: 0}, {
@@ -3529,8 +3532,10 @@ $(document).ready(function(){
             }
           );
         }
-        else if ($(e.target).is($('.card .activator')) ||
-                 $(e.target).is($('.card .activator i')) ) {
+
+        // CUSTOM: Altered the click target
+        else if ($(e.target).is($('.card i')) ) {
+
           $(e.target).closest('.card').css('overflow', 'hidden');
           $(this).find('.card-reveal').css({ display: 'block'}).velocity("stop", false).velocity({translateY: '-100%'}, {duration: 300, queue: false, easing: 'easeInOutQuad'});
         }
